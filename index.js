a131ec7fb1e50a6e3a2b0660fa9a95426124e947
DenDeeZz <swapdesign@yandex.ru>
2018-03-27 20:19:04 -0700

    'GitPushed'

---
+++ index.js
@@ -0,0 +1,10 @@
+// window.onload = () => {
+//   document.getElementById('poi1').onmouseover = () => {
+//     document.getElementById('pop').style.display = 'block';
+//   }
+//
+//   document.getElementById('poi1').onmouseout = () => {
+//     document.getElementById('pop').style.display = 'none';
+//   }
+//
+// }
